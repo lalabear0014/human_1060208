77283f79ce45f2f19345a2ec6d109cd29a976f74
Air <air@Airs-MacBook.local>
2017-02-09 10:03:24 +0800

    Install gem kaminari

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,4 +2,9 @@ class EventsController < ApplicationController
 
 	before_action :authenticate_user!, :except => [:index]
 
+	# GET /events
+	def index
+		@events = Event.page( params[:page] ).per(10)
+	end
+
 end
