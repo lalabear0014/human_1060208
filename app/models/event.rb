1892a462db05a3a8b399bf3bfd266905e940e320
Air <air@Airs-MacBook.local>
2017-03-14 16:58:05 +0800

    Change background-image

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,7 +5,9 @@ class Event < ApplicationRecord
 	belongs_to :user
 	has_many :messages
 
-	has_attached_file :avatar, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
+	has_attached_file :avatar,
+		styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" },
+		:default_url => "egg.png"
   	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
 end
